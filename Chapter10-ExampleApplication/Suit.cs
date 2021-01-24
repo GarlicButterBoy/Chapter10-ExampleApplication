7c1964316e45aec0bc5be571a72f72a991c3f33f
Nick Sturch-Flint <nickdoesreviews@gmail.com>
2021-01-24 11:01:37 -0500

    Add project files.

---
+++ Chapter10-ExampleApplication/Suit.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Chapter10_ExampleApplication
+{
+    public enum Suit
+    {
+        Club,
+        Diamond,
+        Heart,
+        Spade
+    }
+}
